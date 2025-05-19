3d8de7cd1e871c7868b7b0d4817a6570172b279e
Ayush Ranjan <ayushranjan4542@gmail.com>
2025-05-19 17:33:53 +0530

    variable declaration and display

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,21 @@
-console.log("Hello World !!!");
+const accountId = 112244
+let accountEmail = "xyz@gmail.com"
+var accountPassword = "123456"
+let isActive
+accountCity = "Jaipur"
+
+// Prints data in a raw, linear format 
+// Best for general purpose logging - strings, numbers, objects, etc 
+console.log(accountCity)
+console.log([accountId, accountEmail, accountPassword, isActive])
+
+// accountId = 114422  //Will throw an error since const can't be changed
+accountEmail= "abc@gmail.com"
+accountPassword = "987654"
+accountCity = "Jodhpur"
+isActive = true
+
+// displays data in tabular format
+// Best for array of objects, objects with key-value pairs, etc
+console.table([accountId, accountEmail, accountPassword, accountCity, isActive])
+console.table({accountId, accountEmail, accountPassword, accountCity, isActive})
