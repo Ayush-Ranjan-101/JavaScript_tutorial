036d3bcda38f3084cf2a2ce82f39159cb1f37324
Ayush Ranjan <ayushranjan4542@gmail.com>
2025-05-19 18:02:00 +0530

    basic data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+let name = "Ayush"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+// bigint => size > 2^53, when dealing with very large numbers
+// string => ""
+// boolean => true/false
+// null => standalone value of object type:(
+// undefined => denotes empty variables with no data type
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined)
+console.log(typeof null)  // :(
+
+// Can read specifications of js on https://tc39.es/ecma262/ just a little bit
+// mdn is good enough if not that
