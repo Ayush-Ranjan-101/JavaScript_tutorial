32082d802217366cecee0e745bad29f6a4486b72
Ayush Ranjan <ayushranjan4542@gmail.com>
2025-05-19 19:22:24 +0530

    Some conversion and logical errors

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,3 +22,10 @@ let boolean = Boolean(isLoggedIn)
 console.log([boolean, typeof(boolean)])
 
 // There can be more types of conversion operation error
+
+//Treats every other values according to the first value
+console.log("1" + 2 + 2)     //122
+console.log(1 + 2 + "2")    //32
+
+console.log(+true)    //1
+console.log(+"")     //0
