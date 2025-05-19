5f3be75637a4b72171277377249ffddfdaecff99
Ayush Ranjan <ayushranjan4542@gmail.com>
2025-05-19 19:00:39 +0530

    Some conversion operation errors

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33"        //output => 33
+// let score = "33abc"    //output => NaN
+// let score = null      //output => 0
+// let score;           //output => NaN
+// let score = true    //output => 1
+// let score = "xyz"  //output => NaN
+
+console.log([score, typeof(score)])
+
+let valueInNum = Number(score)
+console.log([valueInNum, typeof(valueInNum)])
+
+// let isLoggedIn = 1             //output => true
+// let isLoggedIn = ""           //output => false
+// let isLoggedIn = undefined   //output => false
+// let isLoggedIn = null       //output => false
+let isLoggedIn = NaN          //output => false
+
+console.log([isLoggedIn, typeof(isLoggedIn)])
+
+let boolean = Boolean(isLoggedIn)
+console.log([boolean, typeof(boolean)])
+
+// There can be more types of conversion operation error
