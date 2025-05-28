a29cd349f339130f7e22336de1177a7710ba8a2e
Ayush Ranjan <ayushranjan4542@gmail.com>
2025-05-28 17:11:31 +0530

    simple switch case

---
+++ 04_controlFlow.js/02_switch.js
@@ -0,0 +1,32 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
+
+// break prevents the flow from falling into the other case
+// if deafult is at the end, break keyword is not needed in it
